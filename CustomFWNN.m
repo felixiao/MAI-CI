c24f6943343d6dae787587fdb1237008eae2fccf
Felix <felixiao@users.noreply.github.com>
2021-11-23 10:51:01 +0100

    CustomNetwork

--- CustomFWNN.m
+++
@@ -1,9 +0,0 @@
-function r = CustomFWNN(inputs, outputs)
-    if(nargin == 2)
-        net = feedforwardnet(10);
-        net = train(net,inputs,outputs);
-        view(net);
-        y = net(inputs);
-        r = perform(net,y,outputs);
-    end
-end
